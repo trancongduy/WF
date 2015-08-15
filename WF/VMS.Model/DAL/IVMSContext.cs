ae15e38f881ea9dce2f602222a553bc9f193c4bd
trancongduy <trancongduy0612@gmail.com>
2015-08-15 22:55:02 +0700

    .

---
+++ WF/VMS.Model/DAL/IVMSContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.Model.TeacherModel;
+
+namespace VMS.Model.DAL
+{
+    public interface IVMSContext : IDisposable
+    {
+        DbSet<Teacher> Teachers { get; set; }
+        int SaveChanges();
+        void SetState(object item, EntityState state);
+    }
+}
