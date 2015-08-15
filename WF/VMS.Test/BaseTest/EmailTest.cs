c2ffb785115d384271ef53eb7a89668aa39ef936
trancongduy <trancongduy0612@gmail.com>
2015-08-15 19:19:52 +0700

    .

---
+++ WF/VMS.Test/BaseTest/EmailTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VMS.Model.TeacherModel;
+using VMS.WebApi.Base;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using VMS.Base;
+
+namespace VMS.Test.BaseTest
+{
+    [TestClass]
+    public class EmailTest
+    {
+        [TestMethod]
+        public void Validate_EiuEmail()
+        {
+            #region prepare
+            var testcases = new[]
+            {
+                new{emailString= "duy.tran.k1set@eiu.edu.vn", expected = true},
+                new{emailString= "",  expected = false},      
+                new{emailString= "duy.tran@eiu.edu.vn",expected = true},     
+                new{emailString= "duy.tran@gmail.com",expected = false}, 
+                new{emailString= "duy.tran@yahoo.com",expected = false}, 
+                new{emailString= "@yahoo.com",expected = false}, 
+            };
+            #endregion
+            foreach (var testcase in testcases)
+            {
+                var actual = RegexUtility.IsValidEiuEmail(testcase.emailString);
+                Assert.AreEqual(testcase.expected, actual);
+            }
+        }
+    }
+}
